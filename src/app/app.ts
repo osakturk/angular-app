f036b5079355b83822c29357e03b8936793bcd1d
Omer Akturk <omer.akturk@rabobank.nl>
2025-10-07 15:45:15 +0200

    feat: add user component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Header } from "./header/header";
+import { User } from './user/user';
 
 @Component({
   selector: 'app-root',
-  imports: [Header],
+  imports: [Header, User],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
