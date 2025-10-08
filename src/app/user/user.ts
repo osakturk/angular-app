fb809983d2e120d0cc8ca9e683f0c7c5bde0e8d4
Omer Akturk <omer.akturk@rabobank.nl>
2025-10-08 15:12:19 +0200

    feat: add event emitter

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,4 @@
-import { Component, computed, Input, input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -7,12 +7,16 @@ import { Component, computed, Input, input } from '@angular/core';
   styleUrl: './user.css',
 })
 export class User {
+  @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
+  @Output() selectedUser = new EventEmitter();
 
   get imagePath() {
     return 'users/' + this.avatar;
   }
 
-  onSelectUser() {}
+  onSelectUser() {
+    this.selectedUser.emit(this.id);
+  }
 }
