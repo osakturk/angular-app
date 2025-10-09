a3e7186b8ef107a2c955b7a64cac664a6c9836e0
Omer Akturk <omer.akturk@rabobank.nl>
2025-10-09 13:01:14 +0200

    feat: add tasks component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,25 @@
 import { Component, signal } from '@angular/core';
-import { Header } from "./header/header";
+import { Header } from './header/header';
 import { User } from './user/user';
+import { Tasks } from './tasks/tasks';
 import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, User],
+  imports: [Header, User, Tasks],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('first-angular-app');
-  users = DUMMY_USERS
+  users = DUMMY_USERS;
+  selectedUserId = 'u1'
+  
+  get selectedUser(){
+    return DUMMY_USERS.find((user) => user.id === this.selectedUserId)
+  }
 
-  onSelectUser(id: string){
-    console.log('Selected user with id ' + id)
+  onSelectUser(id: string) {
+    this.selectedUserId = id
   }
 }
