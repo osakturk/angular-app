3a492a12a39c1f07d98a7aebb7489d387e3de2f0
Omer Akturk <omer.akturk@rabobank.nl>
2025-11-07 13:32:32 +0100

    feat: implement complete task button

--- src/app/tasks/task/task.ts
+++ src/app/tasks/task/task.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { type TaskData } from './task.model';
 @Component({
   selector: 'app-task',
@@ -8,5 +8,11 @@ import { type TaskData } from './task.model';
 })
 export class Task {
   @Input({required: true}) taskData!: TaskData;
+  @Output() complete = new EventEmitter();
+
+
+  onCompleteTask(){
+    this.complete.emit(this.taskData.id);
+  }
 
 }
